4b2ac452385d531a868d25cbcc9c8f364a7b2c5a
Grayson1999 <bbt1250912@gmail.com>
2023-09-13 21:40:55 +0900

    ver 1.0.1

--- manage_notion_package/init.py
+++ manage_notion_package/init.py
@@ -17,13 +17,3 @@ headers = {
 datelist = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"]
 last_day_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 leap_year_last_day_of_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-
-
-# ##주차 계산
-# today_week_name = get_today_week_name()
-
-# ##페이지 가져오기
-# past_pages = get_pages(PAST_DATABASE_ID)
-# pages = get_pages(DATABASE_ID)
-# achievement_pages = get_pages(ACHIEVEMENT_DATABASE_ID)
